dbe66f4f0f9ca71cf2ca141acaa699b6cf8a2269
Rohit Singh <rohitsinghchauhan420@gmail.com>
2023-05-18 19:13:16 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <header>Buy Me</header>
+        <main></main>
+        <footer>
+          All right reserved
+        </footer>
+        </div>
+    </>
+  )
+}
+
+export default App
